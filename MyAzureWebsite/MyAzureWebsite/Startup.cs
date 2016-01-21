92c9dbbfdfaa9f143aafcd27050830be31a6c16e
Rasmus Krusaa <rasmuskrusaa@gmail.com>
2016-01-21 20:01:16 +0100

    Setup project

---
+++ MyAzureWebsite/MyAzureWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAzureWebsite.Startup))]
+namespace MyAzureWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
